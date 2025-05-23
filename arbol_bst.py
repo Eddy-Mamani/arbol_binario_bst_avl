58cea42836413ae6b7d23c44333926ef3446ee70
eddy <emamanimamani630@gmail.com>
2025-05-23 01:59:09 -0400

    metodos buscar y eliminar

--- arbol_bst.py
+++ arbol_bst.py
@@ -26,3 +26,39 @@ class ArbolBST:
                 actual = actual.derecho
             else:
                 return 
+            
+    def buscar(self, valor):
+        actual = self.raiz
+        while actual:
+            if valor == actual.valor:
+                return True
+            elif valor < actual.valor:
+                actual = actual.izquierdo
+            else:
+                actual = actual.derecho
+        return False
+
+    def eliminar(self, valor):
+        def _minimo(nodo):
+            while nodo.izquierdo:
+                nodo = nodo.izquierdo
+            return nodo
+
+        def _eliminar(nodo, valor):
+            if nodo is None:
+                return None
+            if valor < nodo.valor:
+                nodo.izquierdo = _eliminar(nodo.izquierdo, valor)
+            elif valor > nodo.valor:
+                nodo.derecho = _eliminar(nodo.derecho, valor)
+            else:
+                if nodo.izquierdo is None:
+                    return nodo.derecho
+                elif nodo.derecho is None:
+                    return nodo.izquierdo
+                temp = _minimo(nodo.derecho)
+                nodo.valor = temp.valor
+                nodo.derecho = _eliminar(nodo.derecho, temp.valor)
+            return nodo
+
+        self.raiz = _eliminar(self.raiz, valor)
